19d7b196730b7a92df0b0d53a6bac54f6e3fd14e
AJAYK-01 <ajayk@cet.ac.in>
2020-08-14 17:41:15 +0530

    Final Working Bot

--- bot.py
+++ bot.py
@@ -2,23 +2,44 @@ from telebot import TeleBot, types
 from decouple import config
 from scrapper import scrape
 import requests
-import os
+import json
+import time
 
 token = config('TOKEN')
 bot = TeleBot(token)
 
 
-def get_contents():
-    contents = scrape()
-    return contents
+def get_contents(chat_id):
+    contents = []
+    scraped = scrape()
+    if scraped != []:
+        js = open("data/"+chat_id+"_data.json","r")
+        datas = json.load(js)
+        for scrap in scraped:
+            k = 0
+            for data in datas:
+            # Can't do a "not in" comparison with dictionary element coz dload links in it are unique to each request
+                if data['title'] == scrap['title']:
+                    k = 1
+                    break   
+            
+            if k == 0:
+                contents.append(scrap)
+        js.close()
+        js = open("data/"+chat_id+"_data.json","w")
+        json.dump(scraped, js, indent=4)
+        js.close()
+        
+        return contents
+    else:
+        return []
 
-@bot.message_handler(commands=["notifs"])
-def send_random_article(message):
+def send_notifs(message):
     """/notifs."""
-    contents = get_contents()
-    i = 0
-    for content in contents:
-        if i < 5:
+    chat_id = str(message.chat.id)
+    contents = get_contents(chat_id)
+    if contents and contents != []:
+        for content in contents:
             msg_content = content['date']+'\n\n'+content["title"]+':\n\n'+content["content"]
             for link in content["link"]:
                 msg_link_text = "<a href=\""+link["url"]+"\">"+link["text"]+"</a>"
@@ -26,16 +47,55 @@ def send_random_article(message):
             bot.send_message(
                 message.chat.id, msg_content, parse_mode="html",
             )
-            i += 1
-        else:
+            
+    else:
+        pass
+
+@bot.message_handler(commands=["view"])
+def fetch_notifs(message):
+    contents = scrape()
+    for i in range(10):
+        content = contents[i]
+        msg_content = content['date']+'\n\n'+content["title"]+':\n\n'+content["content"]
+        for link in content["link"]:
+            msg_link_text = "<a href=\""+link["url"]+"\">"+link["text"]+"</a>"
+            msg_content += "\n"+msg_link_text
+        bot.send_message(
+            message.chat.id, msg_content, parse_mode="html",
+        )
+
+
+@bot.message_handler(commands=["subscribe"])
+def subscribed(message):
+    file1 = open("sub/"+str(message.chat.id)+".txt", "w")
+    file1.write("T")
+    file1.close()
+    while 1:
+        file2 = open("sub/"+str(message.chat.id)+".txt", "r")
+        #checking if unsubscribed
+        if file2.read() == "F":
             break
+        file2.close()
+        send_notifs(message)
+        time.sleep(30)
+                
+@bot.message_handler(commands=["unsubscribe"])
+def unsubscribed(message):
+    file1 = open("sub/"+str(message.chat.id)+".txt", "w")
+    file1.write("F")
+    file1.close()
 
 
 @bot.message_handler(commands=["start", "help"])
 def send_instructions(message):
     """/start, /help"""
+    chat_id = str(message.chat.id)
+    js = open("data/"+chat_id+"_data.json","w")
+    data = scrape()
+    json.dump(data, js, indent=4)
+    js.close()
     msg_content = (
-        "*Available commands:*\n\n" "/notifs - get first 5 ktu announcements (wait some time after giving command)"
+        "*Available commands:*\n\n" "/subscribe - Subscribe to KTU Notifs \n\n /unsubscribe - Unsubscribe from KTU Notifs \n\n /view - Fetch latest 10 Notifs from KTU"
     )
     bot.send_message(
         message.chat.id, msg_content, parse_mode="markdown",
--- scrapper.py
+++ scrapper.py
@@ -3,38 +3,41 @@ from bs4 import BeautifulSoup
 
 def scrape():
     url = "https://ktu.edu.in/eu/core/announcements.htm"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    table = soup.find("table", {"class": "ktu-news"})
-    tr_list = table.findAll("tr")
-    # print(tr_list)
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        table = soup.find("table", {"class": "ktu-news"})
+        tr_list = table.findAll("tr")
 
-    data = []
-    for tr in tr_list:
-        links = []
-        content = tr.findAll("b")
-        try:
-            links_all = tr.findAll("a")
-            for link in links_all:
-                text = link.find(text=True)
-                link = str(link.get('href'))
-                if link.startswith('/'):
-                    link = "https://ktu.edu.in"+link
-                links.append(dict({'url': link, 'text': text}))
-        except:
+        data = []
+        for tr in tr_list:
             links = []
-        date = content[0].text.split(':')[0][:-3]
-        title = content[1].text
+            content = tr.findAll("b")
+            try:
+                links_all = tr.findAll("a")
+                for link in links_all:
+                    text = link.find(text=True)
+                    link = str(link.get('href'))
+                    if link.startswith('/'):
+                        link = "https://ktu.edu.in"+link
+                    links.append(dict({'url': link, 'text': text}))
+            except:
+                links = []
+            date = content[0].text.split(':')[0][:-3]
+            title = content[1].text
 
-        texts = tr.find("li").findAll(text=True)
-        content = ''
-        for text in texts:
-            if len(text) > 22:
-                if text != title:
-                    content = text.replace('\n','').replace('\r','')
-                    break
+            texts = tr.find("li").findAll(text=True)
+            content = ''
+            for text in texts:
+                if len(text) > 25:
+                    if text != title:
+                        content += text.replace('\n','').replace('\r','')+'\n'
+                        # break
 
-        data.append(dict({'date': date, 'title': title, 'link': links, 'content': content}))
-        
+            data.append(dict({'date': date, 'title': title, 'link': links, 'content': content}))
+    
+    except Exception as e:
+        data = []
+        print(str(e))
     # print(data[0])
     return data
