726ab9b3c0cca1c4b727f1d8c45449ca23cdd226
Nanda Kishor M Pai <nandakishormpai@gmail.com>
2021-03-27 00:55:49 +0530

    added new model but not good

--- nlp.py
+++ nlp.py
@@ -19,51 +19,53 @@ from numpy import loadtxt
 from tensorflow.python.keras.models import load_model
 import pickle
 from nltk.stem import WordNetLemmatizer 
+from sklearn.naive_bayes import GaussianNB
+from sklearn import metrics
   
 lemmatizer = WordNetLemmatizer()
 
 # load doc into memory
 def load_doc(filename):
-	# open the file as read only
-	file = open(filename, 'r')
-	# read all text
-	text = file.read()
-	# close the file
-	file.close()
-	return text
+    # open the file as read only
+    file = open(filename, 'r')
+    # read all text
+    text = file.read()
+    # close the file
+    file.close()
+    return text
 
 # turn a doc into clean tokens
 def clean_doc(doc):
-	# split into tokens by white space
-	tokens = doc.split()
-	# remove punctuation from each token
-	tokens = [lemmatizer.lemmatize(w) for w in tokens]
-	# remove remaining tokens that are not alphabetic
-	tokens = [word for word in tokens if word.isalpha()]
-	# filter out stop words
-	stop_words = set(stopwords.words('english'))
-	tokens = [w for w in tokens if not w in stop_words]
-	# filter out short tokens
-	tokens = [word for word in tokens if len(word) > 1]
-	return tokens
+    # split into tokens by white space
+    tokens = doc.split()
+    # remove punctuation from each token
+    tokens = [lemmatizer.lemmatize(w) for w in tokens]
+    # remove remaining tokens that are not alphabetic
+    tokens = [word for word in tokens if word.isalpha()]
+    # filter out stop words
+    stop_words = set(stopwords.words('english'))
+    tokens = [w for w in tokens if not w in stop_words]
+    # filter out short tokens
+    tokens = [word for word in tokens if len(word) > 1]
+    return tokens
 
 # load doc and add to vocab
 def add_doc_to_vocab(filename, vocab):
-	# load doc
-	doc = load_doc(filename)
-	# clean doc
-	tokens = clean_doc(doc)
-	# update counts
-	vocab.update(tokens)
+    # load doc
+    doc = load_doc(filename)
+    # clean doc
+    tokens = clean_doc(doc)
+    # update counts
+    vocab.update(tokens)
 
 # load all docs in a directory
 def process_docs(directory, vocab, is_trian):
-	# walk through all files in the folder
-	for filename in listdir(directory):
-		# create the full path of the file to open
-		path = directory + '/' + filename
-		# add doc to vocab
-		add_doc_to_vocab(path, vocab)
+    # walk through all files in the folder
+    for filename in listdir(directory):
+        # create the full path of the file to open
+        path = directory + '/' + filename
+        # add doc to vocab
+        add_doc_to_vocab(path, vocab)
 
 # define vocab
 vocab = Counter()
@@ -81,27 +83,27 @@ print(len(tokens))
 
 # save list to file
 def save_list(lines, filename):
-	# convert lines to a single blob of text
-	data = '\n'.join(lines)
-	# open file
-	file = open(filename, 'w')
-	# write text
-	file.write(data)
-	# close file
-	file.close()
+    # convert lines to a single blob of text
+    data = '\n'.join(lines)
+    # open file
+    file = open(filename, 'w')
+    # write text
+    file.write(data)
+    # close file
+    file.close()
 
 # save tokens to a vocabulary file
 save_list(tokens, 'vocab.txt')
 
 # load doc into memory
 def load_doc(filename):
-	# open the file as read only
-	file = open(filename, 'r')
-	# read all text
-	text = file.read()
-	# close the file
-	file.close()
-	return text
+    # open the file as read only
+    file = open(filename, 'r')
+    # read all text
+    text = file.read()
+    # close the file
+    file.close()
+    return text
 
 # load the vocabulary
 vocab_filename = 'vocab.txt'
@@ -111,30 +113,34 @@ vocab = set(vocab)
 
 # turn a doc into clean tokens
 def clean_doc(doc, vocab):
-	# split into tokens by white space
-	tokens = doc.split()
-	# remove punctuation from each token
-	tokens = [lemmatizer.lemmatize(w) for w in tokens]
-	# filter out tokens not in vocab
-	tokens = [w for w in tokens if w in vocab]
-	tokens = ' '.join(tokens)
-	return tokens
+    # split into tokens by white space
+    tokens = doc.split()
+    # remove punctuation from each token
+    tokens = [lemmatizer.lemmatize(w) for w in tokens]
+    # filter out tokens not in vocab
+    tokens = [w for w in tokens if w in vocab]
+    tokens = ' '.join(tokens)
+    return tokens
 
-	# load all docs in a directory
+
+    # load all docs in a directory
+
+
 def process_docs(directory, vocab, is_trian):
-	documents = list()
-	# walk through all files in the folder
-	for filename in listdir(directory):
-		# skip any reviews in the test set
-		# create the full path of the file to open
-		path = directory + '/' + filename
-		# load the doc
-		doc = load_doc(path)
-		# clean doc
-		tokens = clean_doc(doc, vocab)
-		# add to list
-		documents.append(tokens)
-	return documents
+    documents = list()
+    print(directory+" : ",len(listdir(directory)))
+    # walk through all files in the folder
+    for filename in listdir(directory):
+        # skip any reviews in the test set
+        # create the full path of the file to open
+        path = directory + '/' + filename
+           # load the doc
+        doc = load_doc(path)
+        # clean doc
+        tokens = clean_doc(doc, vocab)
+        # add to list
+        documents.append(tokens)
+    return documents
 
 
 def make_model():
@@ -142,9 +148,6 @@ def make_model():
     positive_docs = process_docs('data/pos_train', vocab, True)
     negative_docs = process_docs('data/neg_train', vocab, True)
     train_docs = negative_docs + positive_docs
-
-
-
     # create the tokenizer
     tokenizer = Tokenizer()
     # fit the tokenizer on the documents
@@ -163,7 +166,7 @@ def make_model():
     Xtrain = pad_sequences(encoded_docs, maxlen=max_length, padding='post')
 
     # define training labels
-    ytrain = np.array([0 for _ in range(160)] + [1 for _ in range(160)])
+    ytrain = np.array([0 for _ in range(250)] + [1 for _ in range(250)])
 
     # load all test reviews
     positive_docs = process_docs('data/pos_test', vocab, False)
@@ -174,18 +177,21 @@ def make_model():
     # pad sequences
     Xtest = pad_sequences(encoded_docs, maxlen=max_length, padding='post')
     # define test labels
-    ytest = np.array([0 for _ in range(7)] + [1 for _ in range(50)])
+    ytest = np.array([0 for _ in range(len(listdir("data/neg_test")))] + [1 for _ in range(len(listdir("data/pos_test")))])
 
     print("\n pad_sequences : ",Xtest)
     print("\n ytest : ",ytest)
 
-        
     # define vocabulary size (largest integer value)
     vocab_size = len(tokenizer.word_index) + 1
 
     # define model
     model = Sequential()
     model.add(Embedding(vocab_size, 100, input_length=max_length))
+    model.add(Conv1D(filters=128, kernel_size=8, activation='relu'))
+    model.add(MaxPooling1D(pool_size=2))
+    model.add(Conv1D(filters=64, kernel_size=8, activation='relu'))
+    model.add(MaxPooling1D(pool_size=2))
     model.add(Conv1D(filters=32, kernel_size=8, activation='relu'))
     model.add(MaxPooling1D(pool_size=2))
     model.add(Flatten())
@@ -195,22 +201,66 @@ def make_model():
     # compile network
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     # fit network
-    model.fit(Xtrain, ytrain, epochs=15, verbose=1)
+    model.fit(Xtrain, ytrain, epochs=20, verbose=1)
     # evaluate
     loss, acc = model.evaluate(Xtest, ytest, verbose=0)
     print('Test Accuracy: %f' % (acc*100))
 
-
-    model.save("relevancy_model.h5")
+    model.save("relevancy_model_v2.0.h5")
     print("Done!")
 
+def make_model_NB():
+    # load all training reviews
+    positive_docs = process_docs('data/pos_train', vocab, True)
+    negative_docs = process_docs('data/neg_train', vocab, True)
+    train_docs = negative_docs + positive_docs
+    # create the tokenizer
+    tokenizer = Tokenizer()
+    # fit the tokenizer on the documents
+    tokenizer.fit_on_texts(train_docs)
+    with open('tokenizer.pickle', 'wb') as handle:
+        pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    # sequence encode
+    encoded_docs = tokenizer.texts_to_sequences(train_docs)
+
+    # pad sequences
+    max_length = max([len(s.split()) for s in train_docs])
+    print("\n\n maxlenght="+str(max_length))
+
+    from tensorflow.python.keras.preprocessing.sequence import pad_sequences
+    Xtrain = pad_sequences(encoded_docs, maxlen=max_length, padding='post')
+
+    # define training labels
+    ytrain = np.array([0 for _ in range(250)] + [1 for _ in range(250)])
+
+    # load all test reviews
+    positive_docs = process_docs('data/pos_test', vocab, False)
+    negative_docs = process_docs('data/neg_test', vocab, False)
+    test_docs = negative_docs + positive_docs
+    # sequence encode
+    encoded_docs = tokenizer.texts_to_sequences(test_docs)
+    # pad sequences
+    Xtest = pad_sequences(encoded_docs, maxlen=max_length, padding='post')
+    # define test labels
+    ytest = np.array([0 for _ in range(len(listdir("data/neg_test")))] + [1 for _ in range(len(listdir("data/pos_test")))])
+
+    print("\n pad_sequences : ",Xtest)
+    print("\n ytest : ",ytest)
+    gnb = GaussianNB()
+    gnb.fit(Xtrain, ytrain)
+    
+    # making predictions on the testing set
+    y_pred = gnb.predict(Xtest)
+
+    print("Gaussian Naive Bayes model accuracy(in %):", metrics.accuracy_score(ytest, y_pred)*100)
 
 
 def predict_process_docs(doc,vocab):
     documents = list()
-	# clean doc
+    # clean doc
     tokens = clean_doc(doc, vocab)
-	# add to list
+    # add to list
     documents.append(tokens)
     return documents
 
@@ -233,6 +283,9 @@ def predict(doc):
         #print("\nIrrelevant \n")   
         return(0)
 
+
+
+
 def relevant(notif):
     """ Checks for relevance of the notification content """ 
     result=predict(notif)
@@ -241,3 +294,7 @@ def relevant(notif):
         return 1
     else :
         return 0 
+
+
+if __name__ == "__main__":
+    make_model()
